1a80f206ba040258cb68fa228816f7838a6bb0b4
Rasel Hossen <raselhossen052@gmail.com>
2024-11-13 12:22:21 +0600

    modal-fn-complete

--- components/Recipes/SingleRecipe.jsx
+++ components/Recipes/SingleRecipe.jsx
@@ -2,6 +2,20 @@ import { X } from 'lucide-react';
 import Image from 'next/image';
 
 const SingleRecipe = ({ data, onClose }) => {
+  const handleAddToCart = () => {
+    const currentCart = JSON.parse(localStorage.getItem('cart')) || [];
+    const updatedCart = [...currentCart, data];
+    localStorage.setItem('cart', JSON.stringify(updatedCart));
+
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+      const userCart = user.cart || [];
+      user.cart = [...userCart, data];
+
+      localStorage.setItem('user', JSON.stringify(user));
+    }
+    onClose(false);
+  };
   return (
     <div className='flex flex-col gap-5'>
       <div className='flex justify-end'>
@@ -23,7 +37,7 @@ const SingleRecipe = ({ data, onClose }) => {
       <h2 className='text-2xl font-semibold'>{data.strMeal}</h2>
       <div className='flex justify-center'>
         <button
-          onClick={() => onClose(false)}
+          onClick={handleAddToCart}
           className='w-full font-bold py-3 px-6 text-center rounded-full transition bg-yellow-300 hover:bg-yellow-100 active:bg-yellow-400 focus:bg-yellow-300 sm:w-max'
         >
           Add to Cart
