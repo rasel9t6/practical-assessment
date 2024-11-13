b695dea9759567b561a8a835bcfc5496dded0051
Rasel Hossen <raselhossen052@gmail.com>
2024-11-14 00:34:59 +0600

    fix-fetch-url

--- app/api/recipes/getAllRecipes/route.js
+++ app/api/recipes/getAllRecipes/route.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export async function GET() {
   try {
     const response = await axios.get(`${process.env.BASE_URL}/filter.php?a=American`);
-    return new NextResponse.json(response.data.meals || []);
+    return NextResponse.json(response.data.meals || []);
   } catch (error) {
     console.error('Error fetching all recipes:', error);
     return new NextResponse.json(
